19a9698c328ad845a3287e29499526a6612ffddf
rravich <ramkrganesh@gmail.com>
2025-11-13 21:46:08 +0100

    updating the skeleton for 9960 driver

--- 01_9960_Driver/Gesture_Sense.h
+++ 01_9960_Driver/Gesture_Sense.h
@@ -3,4 +3,16 @@
 
 #include "rra_StdTypes.h"
 
+typedef enum {
+    NOTHING = 0u,
+    DOWNWARD,
+    UPWARD,
+}gesture_t;
+
+class Gesture {
+private:
+    gesture_t detection {NOTHING};
+public:
+    gesture_t get_gesture();
+};
 #endif
